584fb628650e8012cc5bc421a9cded8da178973e
Brandon <brandon@hackyhapper.com>
2020-02-08 11:22:24 -0800

    including binariy and dlls

--- compile-windows.sh
+++ compile-windows.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-env CGO_ENABLED="1" CC="/usr/bin/x86_64-w64-mingw32-gcc" GOOS="windows" CGO_LDFLAGS="-lmingw32 -lSDL2" CGO_CFLAGS="-D_REENTRANT" go build -o ./bin/GoGoGadgetWheel.exe -x ./src/main.go 
+env CGO_ENABLED="1" CC="/usr/bin/x86_64-w64-mingw32-gcc" GOOS="windows" CGO_LDFLAGS="-lmingw32 -lSDL2" CGO_CFLAGS="-D_REENTRANT" go build -o ./bin/win/GoGoGadgetWheel.exe -x ./src/main.go 
